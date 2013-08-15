e7ab07c03731b795c54b21c88f6d9a85efa8966c
Paul Grunewald <paul.grunewald@mailbox.tu-dresden.de>
2013-08-15 15:27:47 +0000

    Fixed timestamp for messages
    git-svn-id: http://developtools.mz.tu-dresden.de/svn/tudwcms/tud.addons.chat/trunk@2641 efab85e1-9c80-4ee9-a28c-d7e3c3e775e7

--- TUDChat/skins/TUDChat/tud_chat_user.js
+++ TUDChat/skins/TUDChat/tud_chat_user.js
@@ -1,11 +1,11 @@
 function printMessage(message) {
 
-    changes = applyAttributes(message.message, message.attributes);    
+    changes = applyAttributes(message.message, message.attributes);
     parsed_message = hyperlink(changes.message_content);
     entry_classes = changes.entry_classes + ((message.name == ownUsername) ? ' ownMessage' : '');
     additional_content = changes.additional_content;
-        
-    return "<li id=chatEntry"+message.id+" class='"+entry_classes+"'><span class='username'>"+message.name+message.date+":</span> <span class='message_content'>"+parsed_message+"</span> "+(additional_content != '' ? "<span class='additional_content'>"+additional_content+"</span>" : "")+"</li>";
+
+    return "<li id=chatEntry"+message.id+" class='"+entry_classes+"'><span class='chatdate'>"+message.date+"</span><span class='username'>"+message.name+":</span> <span class='message_content'>"+parsed_message+"</span> "+(additional_content != '' ? "<span class='additional_content'>"+additional_content+"</span>" : "")+"</li>";
 }
 
 function printNewUser(user) {
