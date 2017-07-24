1ccabaa27dbe2cdf460bfc1dba299f2c6423465f
David Voigt <david.voigt@tu-dresden.de>
2017-07-24 21:17:16 +0200

    WEBCMS-1906: Introduce permissions for chat and chat session creation

--- tud/addons/chat/__init__.py
+++ tud/addons/chat/__init__.py
@@ -26,13 +26,15 @@ def initialize(context):
     content_types, constructors, ftis = process_types(listOfTypes, 'tud.addons.chat')
     logger.info('content_types, constructors, ftis : %s' % str((content_types, constructors, ftis,)))
 
-    ret = ContentInit('%s Content' % 'tud.addons.chat',
-                content_types = tuple(content_types),
-                extra_constructors = tuple(constructors),
-                fti = ftis,
-                ).initialize(context)
+    for atype, constructor, fti in zip(content_types, constructors, ftis):
+        ret = ContentInit('%s: %s' % ('tud.addons.chat', atype.portal_type),
+                          content_types=(atype,),
+                          extra_constructors=(constructor,),
+                          fti=(fti,),
+                          permission=ADD_PERMISSIONS[atype.portal_type],
+                         ).initialize(context)
 
-    logger.info('return value of ContentInit: %s' % str(ret))
+        logger.info('return value of ContentInit: %s' % str(ret))
 
     tool = ToolInit('%s Tool' % PROJECTNAME,
                     tools=(TUDChatTool,),
--- tud/addons/chat/config.py
+++ tud/addons/chat/config.py
@@ -7,3 +7,8 @@ GLOBALS = globals()
 PROJECTNAME = 'TUDChat'
 SKINS_DIR = 'skins'
 TOOL_ID = 'portal_tud_chat'
+
+ADD_PERMISSIONS = {
+    "Chat": "tud.addons.chat: Add Chat",
+    "ChatSession": "tud.addons.chat: Add ChatSession",
+}
